c11788e07f7317791f563dd8d75390afdbf7b4ab
tinray <b03902072@ntu.edu.tw>
2016-11-11 10:58:20 +0800

    finish factor-graph implementation

---
+++ src/factor-graph.cpp
@@ -0,0 +1,37 @@
+#include "factor-graph.hpp"
+#include <cassert>
+
+FactorGraph::FactorGraph(unsigned int nVars)
+    :nVars(nVars){}
+
+
+void FactorGraph::addFactor(const std::vector<Index>&scope,
+                            const FactorFunction&factorFunction) {
+    FactorGraph::Factor factor {
+        scope, factorFunction
+    };
+    factors.push_back(factor);
+    for (auto ind: scope) {
+        assert(ind < nVars);
+        varFactors[ind].push_back(factors.size() - 1);
+    }
+}
+
+
+Real FactorGraph::evalFactor(Index factorIndex,
+                             const std::vector<Label>&assignment){
+    std::vector<Label>args;
+    for (auto varIndex: factors[factorIndex].scope) {
+        args.push_back(assignment[varIndex]);
+    }
+    return factors[factorIndex].function.eval(args);
+}
+
+Real FactorGraph::evalAt(Index varIndex, 
+                         const std::vector<Label>&assignment){
+    Real potential = RealMulId;
+    for (auto varFactor: varFactors[varIndex]) {
+        potential *= evalFactor(varFactor, assignment);
+    }
+    return potential;
+}
--- src/factor-graph.hpp
+++ src/factor-graph.hpp
@@ -12,10 +12,13 @@ private:
     };
     std::vector<std::vector<Index>>varFactors;
     std::vector<Factor>factors;
-
+    Real evalFactor(Index factorIndex,
+                    const std::vector<Label>&assignment);
 public:
     FactorGraph(unsigned int nVars);
     void addFactor(const std::vector<Index>&scope,
-                   const FactorFunction&factorFunction);                     
+                   const FactorFunction&factorFunction);
+    Real evalAt(Index varIndex,
+                const std::vector<Label>&assignment);
 };
 
