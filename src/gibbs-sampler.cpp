1559ff856c8b7806348758ebb2cce6e2f8deeac5
tinray <b03902072@ntu.edu.tw>
2016-11-11 17:32:40 +0800

    implementing gibbs-sampler: only check converge remain to be done

--- src/gibbs-sampler.cpp
+++ src/gibbs-sampler.cpp
@@ -1,4 +1,58 @@
 #include "gibbs-sampler.hpp"
+#include <random>
+
+std::random_device rd;
+std::mt19937_64 gen(rd());    
+
+
+Label rand(Real potenrialP, Real potenrialN){
+    std::bernoulli_distribution d(potenrialP / (potenrialN + potenrialP));
+    return d(gen) ? Label::positive : Label::negative;
+}
+
+GibbsSampler::Chain::Chain(const FactorGraph&factorGraph,
+                           const std::vector<TargetFunction>&targetFunctions)
+    :factorGraph(factorGraph),
+     targetFunctions(targetFunctions),
+     assignment(std::vector<Label>(factorGraph.nVars)),
+     means(std::vector<Real>(targetFunctions.size(), RealAddId)),
+     vars(std::vector<Real>(targetFunctions.size(), RealAddId)),
+     results(targetFunctions.size(), std::vector<Real>())
+{
+    srand(time(NULL));
+    for (auto i = 0u; i < assignment.size(); ++i){
+        assignment[i] = (rand() & 1) == 0 ? Label::positive : Label::negative;
+    }
+}
+
+
+void GibbsSampler::Chain::init(unsigned int nIgnore){
+    for (auto i = 0u; i < nIgnore; ++i) {
+        step();
+    }
+}
+
+
+void GibbsSampler::Chain::iterate(unsigned int nIter){
+    for (auto i = 0u; i < nIter; ++i) {
+        step();
+        for (auto j = 0u; j < targetFunctions.size(); ++j) {
+            results[j].push_back(targetFunctions[j].eval(assignment));
+        }
+    }
+}
+
+
+void GibbsSampler::Chain::step(){
+    for (auto i = 0u; i < factorGraph.nVars; ++i ){
+        assignment[i] = Label::positive;
+        Real potentialP = factorGraph.evalAt(i, assignment);
+        assignment[i] = Label::negative;
+        Real potentialN = factorGraph.evalAt(i, assignment);
+        assignment[i] = rand(potentialP, potentialN);
+    }
+}
+
 
 GibbsSampler::GibbsSampler(const std::vector<TargetFunction>&targetFunctions,
                            const FactorGraph&factorGraph)
@@ -6,25 +60,25 @@ GibbsSampler::GibbsSampler(const std::vector<TargetFunction>&targetFunctions,
      factorGraph(factorGraph){}
 
 
-std::vector<long double> GibbsSampler::doSample(unsigned int nChains,
+std::vector<Real> GibbsSampler::doSample(unsigned int nChains,
                                                 unsigned int nIgnore,
                                                 long double convergeRatio){
-    std::vector<Chain>chains(nChains);
+    std::vector<Chain>chains(nChains, Chain(factorGraph, targetFunctions));
 
     // burn-in step
     for (auto&& chain: chains) {
-        chain.init(nIgnore, factorGraph);
+        chain.init(nIgnore);
     }
 
     // start sampling
     while (!isConverge(convergeRatio, chains)){
         for (auto&& chain: chains){
-            chain.iterate(1, factorGraph, targetFunctions);
+            chain.iterate(1);
         }
     }
 
     // average for result
-    std::vector<long double>means(targetFunctions.size());
+    std::vector<Real>means(targetFunctions.size());
     for (auto&& chain: chains) {
         auto&m = chain.getMeans();
         for (auto i = 0u; i < means.size(); ++i) {
--- src/gibbs-sampler.hpp
+++ src/gibbs-sampler.hpp
@@ -7,26 +7,27 @@ private:
     const FactorGraph&factorGraph;
     class Chain{
     private:
+        const FactorGraph&factorGraph;
+        const std::vector<TargetFunction>&targetFunctions;
         unsigned int nItered;
-        void step(Index varIndex, std::vector<Label>&chain);
-        std::vector<long double>means;
-        std::vector<long double>vars;
+        std::vector<Label>assignment;        
+        std::vector<Real>means, vars;
+        std::vector<std::vector<Real>>results;
+        void step();
     public:
-        Chain();
-        void init(unsigned int nIgnore,
-                  const FactorGraph&factorGraph);
-        void iterate(unsigned int nIter,
-                     const FactorGraph&factorGraph,
-                     const std::vector<TargetFunction>&targetFunctions);
-        const std::vector<long double>getMeans();
-        const std::vector<long double>getVars();
+        Chain(const FactorGraph&factorGraph,
+              const std::vector<TargetFunction>&targetFunctions);
+        void init(unsigned int nIgnore);
+        void iterate(unsigned int nIter);
+        const std::vector<Real>getMeans();
+        const std::vector<Real>getVars();
     };
     bool isConverge(long double ratio,
                        const std::vector<Chain>&chains);
 public:
     GibbsSampler(const std::vector<TargetFunction>&targetFunctions,
                  const FactorGraph&factorGraph);
-    std::vector<long double> doSample(unsigned int nChains,
+    std::vector<Real> doSample(unsigned int nChains,
                                unsigned int nIgnore,
                                long double convergeRatio);
 };
