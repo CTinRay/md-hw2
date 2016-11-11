24136ea1797e6c6fef43bae8f74955056cb0a8c2
tinray <b03902072@ntu.edu.tw>
2016-11-11 09:39:38 +0800

    remove def of factor function from graph.hpp

--- src/graph.hpp
+++ src/graph.hpp
@@ -4,12 +4,12 @@ typedef long double Real;
 typedef short Label;
 typedef unsigned int Index;
 
-class FactorFunction{
+class FactorFunction {
 public:
     virtual Real eval(const std::vector<Label>&args) const;
 };
 
-class FactorGraph{
+class FactorGraph {
 public:
     unsigned int nVars;
     FactorGraph(unsigned int nVars);
@@ -20,47 +20,3 @@ public:
 
 };
 
-
-class GFactorFunction: FactorFunction{
-public:
-    Real beta;
-    GFactorFunction(Real beta);
-    Real eval(const std::vector<Label>&args) const{
-        return expl(beta * args[0] * args[1]);
-    }
-};
-
-
-class FFactorFunction: FactorFunction{
-    static std::vector<Real>alpha;
-    std::vector<Real>&features;
-    FFactorFunction(const std::vector<Real>&features);
-    Real eval(const std::vector<Label>&args) const{
-        return expl(args[0] * (alpha[0] * features[0] +
-                               alpha[1] * features[1] +
-                               alpha[2] * features[2] ) );
-    }
-};
-
-
-
-class HFactorFunction: FactorFunction{
-    static Real gamma;
-    unsigned int ti;
-    std::vector<Real>&features;
-    HFactorFunction(unsigned int ti);
-    Real eval(const std::vector<Label>&args) const{
-        unsigned int count = 0;
-        for (unsigned int i = 0; i < args.size(); ++i ){
-            count += args[i] == 1 ? 1 : 0;
-        }
-        return expl(gamma * (ti - count) * (ti - count));
-    }
-};
-
-int funct(){
-    CCFactorFunction* OI = new CCFactorFunction(1);
-    CCFactorFunction* FI = new CCFactorFunction(1);
-    CCFactorFunction* OF = new CCFactorFunction(1);
-    
-}
