00c2e77a0c52fecb947b27e6316b392779269730
tinray <b03902072@ntu.edu.tw>
2016-11-15 17:33:04 +0800

    PLAN III initial commit

---
+++ src3/plan3.py
@@ -0,0 +1,44 @@
+import numpy as np
+import argparse
+
+
+def load_feature(filename):
+    f = open(filename)
+    features = []
+    for l in f:
+        features += [list(map(float, l.strip().split(' ')))]
+    return np.array(features)
+    
+
+def main():
+    parser = argparse.ArgumentParser(description='===== PLAN III =====')
+    parser.add_argument('features', type=str, help='features')
+    parser.add_argument('output', type=str, help='output')
+    parser.add_argument('--converge', type=float, help='converge, default = 0.001', default=0.001)
+    parser.add_argument('--verbose', type=bool, help='verbose, default = False', default=False)    
+    parser.add_argument('--learning_rate', type=float, help='learning rate, default = 0.01', default=0.001)    
+    parser.add_argument('--answer', type=str, help='answer')    
+    
+    args = parser.parse_args()
+
+    answer = None
+    if args.answer is not None:
+        f = open(args.answer)
+        answer = np.array(list(map(int, f.read().strip().split('\n'))), dtype=int)
+        
+    features = load_feature(args.features) #[:,[0, 1, 3, 4, 6]]
+    # do normalization
+    features_mean = np.average(features, axis=0)
+    features_var = np.sum((features - features_mean)**2, axis=0)
+    features = (features - features_mean) / (features_var) ** 0.5
+
+    # todo 
+
+    f = open(args.output, 'w')
+    ys = plan2.predict()
+    for y in ys:
+        f.write(str(int(y)) + '\n')
+    print(np.sum(ys == answer) / ys.shape[0])
+
+if __name__ == '__main__':
+    main()
