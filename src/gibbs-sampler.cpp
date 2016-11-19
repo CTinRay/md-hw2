a13795a5ca39d9d3049c2e0148d535d5d3c5ebf4
CTinRay <b03902072@linux7.csie.ntu.edu.tw>
2016-11-19 10:48:41 +0800

    fix bug of var clac

--- src/gibbs-sampler.cpp
+++ src/gibbs-sampler.cpp
@@ -1,6 +1,7 @@
 #include "gibbs-sampler.hpp"
 #include <random>
 #include <thread>
+#include <iostream>
 
 #define SQUARE(X) (X) * (X)
 
@@ -89,32 +90,41 @@ GibbsSampler::GibbsSampler(const std::vector<TargetFunction*>&targetFunctions,
 
 bool GibbsSampler::isConverge(long double convergeRatio,
                               const std::vector<GibbsSampler::Chain>&chains){
-    std::vector<Real>interChainVars(targetFunctions.size());
+    std::vector<Real>interChainMeans(targetFunctions.size(), 0);
+    std::vector<Real>interChainVars(targetFunctions.size(), 0);
     std::vector<Real>intraChainVars(targetFunctions.size());
 
     // mean of mean of chains
     for (auto&&chain: chains) {
         const auto&means = chain.getMeans();
         for (auto i = 0u; i < means.size(); ++i) {
-            interChainVars[i] += means[i] / chains.size();
+            interChainMeans[i] += means[i] / chains.size();
         }
     }
 
     for (auto&&chain: chains) {
         const auto&means = chain.getMeans();
+        const auto&vars = chain.getVars();
         // variance of mean of chains
         for (auto i = 0u; i < means.size(); ++i) {
-            interChainVars[i] += SQUARE(means[i] - interChainVars[i]) / chains.size();
+            interChainVars[i] += SQUARE(means[i] - interChainMeans[i]) / chains.size();
         }
         // average of variance of chains
         for (auto i = 0u; i < means.size(); ++i) {
-            intraChainVars[i] += means[i] / chains.size();
+            intraChainVars[i] += vars[i] / chains.size();
         }        
     }
 
     convergeRatio = SQUARE(convergeRatio);
     for (auto i = 0u; i < intraChainVars.size(); ++i) {
         if (interChainVars[i] / intraChainVars[i] > convergeRatio) {
+            // std::cout << "interchain " << i << " var = " << interChainVars[i] << std::endl;
+            // std::cout << "intrachain " << i << " var = " << intraChainVars[i] << std::endl;
+
+            for (auto&&chain: chains) {
+                std::cout << "mean i" << chain.getMeans()[i] << std::endl;
+            }
+            
             return false;
         }            
     }
@@ -136,18 +146,22 @@ std::vector<Real> GibbsSampler::doSample(unsigned int nChains,
     }
     
     // start sampling
-    for (auto&& chain: chains){
-        chain.iterate(10);
-    }
+    // for (auto&& chain: chains){
+    //     chain.iterate(10);
+    // }
 
-    while (!isConverge(convergeRatio, chains)) {
+
+    do {
+        // std::cout << "100 iter start" << std::endl;
         for (auto i = 0u; i < chains.size(); ++i) {
-            threads[i] = std::thread(&Chain::iterate, &chains[i], 10);
+            threads[i] = std::thread(&Chain::iterate, &chains[i], 100);
         }
         for (auto&&thread: threads) {
             thread.join();
         }
-    };
+        // std::cout << "100 iter end" << std::endl;
+        
+    } while (!isConverge(convergeRatio, chains));
 
 
     // average for result
